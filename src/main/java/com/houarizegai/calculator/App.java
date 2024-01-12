99c411e60e9b7d73574da16652fad2c56103dc47
Cloud User <ec2-user@ip-172-31-0-251.ap-south-1.compute.internal>
2024-01-12 11:06:49 +0000

    Adding a print statement in App.java file

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -5,6 +5,7 @@ import com.houarizegai.calculator.ui.CalculatorUI;
 public class App {
 
     public static void main(String[] args) {
+	System.out.println("Welcome to Java Calculator program");
         new CalculatorUI();
     }
 }
